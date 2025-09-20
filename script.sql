df21ce21954591f100489410acee52be5df6ace4
IdonNeedNAME <1992140645@qq.com>
2025-09-20 19:10:13 +0800

    增添了对伪List的实现，增添了对图片数据的支持，添加了mysql文件，等等

---
+++ script.sql
@@ -0,0 +1,61 @@
+create table comment
+(
+    id         int auto_increment
+        primary key,
+    content    varchar(1000) not null,
+    depth      int           not null,
+    subcomment json          null,
+    likelist   json          null,
+    hidden     int           null,
+    host       int           not null
+);
+
+create table picture
+(
+    id          int auto_increment
+        primary key,
+    pixels      mediumblob not null,
+    width       int        not null,
+    height      int        not null,
+    featurecode int        not null
+);
+
+create table post
+(
+    id         int auto_increment
+        primary key,
+    host       int           not null,
+    title      varchar(20)   not null,
+    content    varchar(1000) not null,
+    subcomment json          null,
+    likelist   json          not null,
+    picture    json          not null,
+    depth      int           not null,
+    anonymity  tinyint(1)    not null,
+    hidden     tinyint(1)    not null,
+    ispublic   tinyint(1)    not null
+);
+
+create table report
+(
+    id        int auto_increment
+        primary key,
+    targetype int           not null,
+    content   varchar(1000) not null,
+    status    int           not null,
+    result    int           not null
+);
+
+create table user
+(
+    id         int auto_increment
+        primary key,
+    username   varchar(20) not null,
+    name       varchar(20) not null,
+    passWord   varchar(30) not null,
+    type       int         not null,
+    pictureref int         not null,
+    blacklist  json        null
+);
+
+

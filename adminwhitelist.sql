6413bc76962d592541137ab96115651e7b538311
IdonNeedNAME <1992140645@qq.com>
2025-09-21 17:03:43 +0800

    添加了白名单的sql文件

---
+++ adminwhitelist.sql
@@ -0,0 +1,8 @@
+create table adminwhitelist
+(
+    id       int auto_increment
+        primary key,
+    username varchar(20) not null
+);
+
+
--- script.sql
+++ script.sql
@@ -1,3 +1,10 @@
+create table adminwhitelist
+(
+    id       int auto_increment
+        primary key,
+    username varchar(20) not null
+);
+
 create table comment
 (
     id         int auto_increment
